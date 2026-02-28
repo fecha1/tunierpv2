5ac921819c4cd7aca2c4369947722d6fb7a33ea8
talelbrini3 <95152604+talelbrini3@users.noreply.github.com>
2026-02-28 22:40:47 +0100

    fix

--- front/react/src/components/Loader.jsx
+++ front/react/src/components/Loader.jsx
@@ -1,15 +1,32 @@
-// @mui
-import Stack from '@mui/material/Stack';
-
-// @assets
-import PageLoader from '@/images/graphics/PageLoader';
-
-/***************************  COMMON - LOADER  ***************************/
+/***************************  COMMON - WIFI LOADER  ***************************/
 
 export default function Loader() {
   return (
-    <Stack sx={{ height: '100vh', width: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <PageLoader />
-    </Stack>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100%',
+        background: '#fff'
+      }}
+    >
+      <div id="wifi-loader">
+        <svg className="circle-outer" viewBox="0 0 86 86">
+          <circle className="back" cx="43" cy="43" r="40" />
+          <circle className="front" cx="43" cy="43" r="40" />
+        </svg>
+        <svg className="circle-middle" viewBox="0 0 60 60">
+          <circle className="back" cx="30" cy="30" r="27" />
+          <circle className="front" cx="30" cy="30" r="27" />
+        </svg>
+        <svg className="circle-inner" viewBox="0 0 34 34">
+          <circle className="back" cx="17" cy="17" r="14" />
+          <circle className="front" cx="17" cy="17" r="14" />
+        </svg>
+        <div className="text" data-text="Chargement" />
+      </div>
+    </div>
   );
 }
